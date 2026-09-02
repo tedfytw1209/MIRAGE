e40c409e9e888c895f3958965e282db8a9cd930e
teddy <teddytw1209@gmail.com>
2026-09-01 20:32:45 -0400

    Update run_bscan_all_tasks.sh

--- run_bscan_all_tasks.sh
+++ run_bscan_all_tasks.sh
@@ -45,9 +45,7 @@ launch() {
         --version v1 \
         --seed 0 \
         --weights \
-            $WEIGHTS_BASE \
             $WEIGHTS_LARGE \
-        $PROBE_FLAG \
         --data_root \
             $DATA_ROOT \
         --csv_root \
